527750464d6b4a8c6e947724ce4de1d1debb122e
KirillKoml <144651672+KirillKoml@users.noreply.github.com>
2024-07-04 19:54:14 +0500

    Update urls.py

--- catalogapp/urls.py
+++ catalogapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from catalogapp.apps import CatalogappConfig
 from catalogapp.views import home
 
-catalog_app = CatalogappConfig.name
+app_name = CatalogappConfig.name
 
 urlpatterns = [
     path('', home, name='home')
