af0b941da2be078d152254759e768c0d79f65652
ps'killer <komliev1999@mail.ru>
2024-07-23 20:06:25 +0500

    2.2

--- catalogapp/models.py
+++ catalogapp/models.py
@@ -1,40 +1,38 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(
         max_length=200,
-        verbose_name="Наименование",
-        help_text="Введите название товара"
+        verbose_name="Категория",
+        help_text="Введите название категорий",
     )
     description = models.TextField(
         verbose_name="Описание",
-        help_text="Введите описание товара",
+        help_text="Введите описание категорий",
         blank=True,
         null=True,
     )
 
     class Mete:
-        verbose_name = 'Товар'
-        verbose_description = 'Товара'
-
+        verbose_name = "Товар"
+        verbose_description = "Товара"
 
     def __str__(self):
         return self.name
+
+
 class Product(models.Model):
-    name = models.ForeignKey(
-        Category,
-        on_delete=models.SET_NULL,
-        verbose_name="Наименование",
-        help_text="Введите название товара",
-        blank=True,
-        null=True,
-        related_name="products",
-    )
     description = models.TextField(
         verbose_name="Описание",
         help_text="Введите описание товара",
         blank=True,
         null=True,
+    )
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Название",
+        help_text="Введите название товара",
     )
     image = models.ImageField(
         upload_to="products/image",
@@ -43,39 +41,38 @@ class Product(models.Model):
         verbose_name="Изображение",
         help_text="Загрузите изображение товара",
     )
-    category = models.CharField(
-        max_length=200,
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
         verbose_name="Категория",
-        help_text="Введите категорию товара"
+        help_text="Введите категорию товара",
+        blank=True,
+        null=True,
+        related_name="products",
     )
     price = models.IntegerField(
-        blank=True, null=True,
-        verbose_name='Цена',
-        help_text='Укажите цену'
+        blank=True, null=True, verbose_name="Цена", help_text="Укажите цену"
     )
-    data_bd = models.DateTimeField(
+    data_bd = models.DateField(
         blank=True,
         null=True,
-        verbose_name='дата создания',
-        help_text='укажите дату создания'
+        verbose_name="дата создания",
+        help_text="укажите дату создания",
     )
-    data_refresh = models.DateTimeField(
+    data_refresh = models.DateField(
         blank=True,
         null=True,
-        verbose_name='дата изменения',
-        help_text='укажите дату изменения0'
+        verbose_name="дата изменения",
+        help_text="укажите дату изменения",
     )
 
     class Mete:
-        verbose_name = 'Товар'
-        verbose_description = 'Товара'
-        ordering = ['name', 'description']
+        verbose_name = "Товар"
+        verbose_description = "Товара"
+        ordering = ["name", "description"]
 
     def __str__(self):
         return self.name
-
-
-
 
 
 # Create your models here.
--- catalogapp/urls.py
+++ catalogapp/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from Django.catalogapp.apps import CatalogappConfig
-from Django.catalogapp.views import home, contact
+from catalogapp.apps import CatalogappConfig
+from catalogapp.views import home, contact
 
 app_name = CatalogappConfig.name
 
-urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contact, name='contact')
-]
+urlpatterns = [path("", home, name="home"), path("contacts/", contact, name="contact")]
